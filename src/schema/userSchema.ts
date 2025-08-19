abb3f84596dc8b4a26cf898cef49a509348a3a3d
Alexia A. de Lima <alexiaalima96@gmail.com>
2025-08-19 11:48:02 -0300

    schama consertado

--- src/schema/userSchema.ts
+++ src/schema/userSchema.ts
@@ -2,13 +2,13 @@ import joi from 'joi';
 
 export const signUpSchema = joi.object({
     name: joi.string(),
-    email: joi.string().email().required,
+    email: joi.string().email().required(),
     password: joi.string().min(8).required()
 
 })
 
 export const signInSchema = joi.object({
-    email: joi.string().email().required,
+    email: joi.string().email().required(),
     password: joi.string().min(8).required()
 
 })
