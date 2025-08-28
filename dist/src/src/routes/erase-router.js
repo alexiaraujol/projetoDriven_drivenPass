de8b9b16b026951fc0d26a471a5d89df422b348a
Alexia A. de Lima <alexiaalima96@gmail.com>
2025-08-28 19:24:34 -0300

    feito

--- dist/src/src/routes/erase-router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const eraseController_1 = require("../controllers/eraseController");
-const tokenMiddleware_1 = require("../middleware/tokenMiddleware");
-const eraseRouter = (0, express_1.Router)();
-eraseRouter.delete("/user/:id", tokenMiddleware_1.authenticateToken, eraseController_1.eraseUsers);
-exports.default = eraseRouter;
--- dist/src/src/services/eraseService.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.eraseUserByIdService = eraseUserByIdService;
-const eraseRepositorie_1 = require("../repositories/eraseRepositorie");
-async function eraseUserByIdService(userId) {
-    await (0, eraseRepositorie_1.deleteUserById)(userId);
-}
